58ed1bf749a8cc2fbfc97ac07367ad676b8257ab
Andrew Boren <andrew.boren@gmail.com>
2012-07-19 22:06:41 -0700

    Qcom/Audio: Import qsd8k audio HAL for jellybean.

--- Android.mk
+++ Android.mk
@@ -21,7 +21,9 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
     include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
 endif
-
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    include $(AUDIO_HW_ROOT)/qsd8k/Android.mk
+endif
 endif
 endif
 endif
